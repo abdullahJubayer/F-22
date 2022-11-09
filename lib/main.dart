a2def917f1a0c2050242f7f3a6adfab6bb372b87
Abdullah jubayer <abdullahjubayer38@gmail.com>
2022-11-10 02:36:49 +0600

    Complete Lab03

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'Lab03.dart';
+
+void main() {
+  runApp(const StartPoint());
+}
+
+class StartPoint extends StatelessWidget {
+  const StartPoint({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Lab03();
+  }
+}
